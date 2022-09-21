84988d3b1415013a430c85b06ce5aecc8922b0c3
Ousman-Busari <usman.traid@gmail.com>
2022-09-21 16:31:12 -0400

    fixed

--- 0x01-python-if_else_loops_functions/13-sorted/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-sorted/13-insert_number.c
@@ -11,6 +11,9 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp, *temp1, *new_node;
 
+	if (*head == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(listint_t));
 	if(!new_node)
 		return (NULL);
@@ -18,16 +21,12 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (*head == NULL)
-		return (NULL);
-
 	if ((*head)->next == NULL && number > (*head)->n)
 	{
 		(*head)->next = new_node;
 		return (new_node);
 	}
-
-	if ((*head)->next == NULL && number < (*head)->n)
+	else if ((*head)->next == NULL && number < (*head)->n)
 	{
 		new_node->next = *head;
 		return (new_node);
@@ -37,19 +36,12 @@ listint_t *insert_node(listint_t **head, int number)
 	temp1 = (*head)->next;
 	while (temp && temp1)
 	{
-		if ((*head)->n > number)
-		{
-			new_node->next = *head;
-			return (new_node);
-		}
-
 		if (temp->n < number && temp1->n > number)
 		{
 			temp->next = new_node;
 			new_node->next = temp1;
 			return (new_node);
 		}
-
 		temp = temp1;
 		temp1 = temp1->next;
 	}
