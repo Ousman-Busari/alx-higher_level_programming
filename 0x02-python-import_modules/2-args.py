507f4b30dfb7e9fa8e27fcf1cc100317e317f9dd
Ousman-Busari <usman.traid@gmail.com>
2022-09-22 11:04:08 -0400

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ argv = sys.argv
 if __name__ == "__main__":
     argc = len(argv)
     if argc == 1:
-        print("{} arguments.".format(--argc))
+        print("{} arguments.".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
