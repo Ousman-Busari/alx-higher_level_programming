a614a2934a02f5bd632405cb40703fc5ef2df27e
Ousman-Busari <usman.traid@gmail.com>
2022-09-26 12:59:09 -0400

    fixed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,8 @@
 
 def replace_in_list(my_list, idx, element):
 
-    max_idx = len(my_list) - 1
+    max_idx = len(my_list)
 
-    if idx >= 0 and idx <= max_idx:
+    if idx >= 0 and idx < max_idx:
         my_list[idx] = element
         return my_list
