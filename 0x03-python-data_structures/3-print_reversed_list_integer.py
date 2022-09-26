94b1a7318ce1b2743c8be3eef7d89cfac645f661
Ousman-Busari <usman.traid@gmail.com>
2022-09-26 12:45:37 -0400

    path fixed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,5 @@ def print_reversed_list_integer(my_list=[]):
 #    if isinstance(my_list, list):
     i = len(my_list)
     while i:
-        print("{:d}".format(my_list[i - 1)))
+        print("{:d}".format(my_list[i - 1]))
         i -= 1
