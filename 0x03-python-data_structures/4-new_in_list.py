98db59776e97a87ec93297a0cdbfae4dda7c4d0a
Ousman-Busari <usman.traid@gmail.com>
2022-09-26 12:27:32 -0400

    alt method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 
 
 def new_in_list(my_list, idx, element):
-    max_idx = len(my_list) - 1
-    if idx >= 0 and idx <= max_idx:
+    max_idx = len(my_list)
+    if idx >= 0 and idx < max_idx:
         new_list = my_list.copy()
         new_list[idx] = element
         return new_list
