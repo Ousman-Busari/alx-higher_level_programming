27fa93e844d3658ba85dcb82ea0ddde812886b11
Ousman-Busari <usman.traid@gmail.com>
2022-09-28 19:24:16 -0400

    instance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,7 @@ def roman_to_int(roman_string):
 
     roman_int = {'M': 100, 'D': 500, 'C': 100, 'L': 50,
                  'X': 10, 'V': 5, 'I': 1}
+
     if roman_string is None or not isinstance(roman_string, str):
         return 0
 
@@ -10,17 +11,15 @@ def roman_to_int(roman_string):
         if roman_int.get(roman_string[i], 0) == 0:
             return 0
 
-    if roman_string is not None:
+    roman_digits = list(roman_string)
+    integer = 0
+    size = len(roman_digits)
 
-        roman_digits = list(roman_string)
-        integer = 0
-        size = len(roman_digits)
+    for i in range(size - 1):
+        if roman_int[roman_digits[i]] < roman_int[roman_digits[i + 1]]:
+            integer += roman_int[roman_digits[i]] * -1
+        else:
+            integer += roman_int[roman_digits[i]]
 
-        for i in range(size - 1):
-            if roman_int[roman_digits[i]] < roman_int[roman_digits[i + 1]]:
-                integer += roman_int[roman_digits[i]] * -1
-            else:
-                integer += roman_int[roman_digits[i]]
-
-        integer += roman_int[roman_digits[size - 1]]
-        return integer
+    integer += roman_int[roman_digits[size - 1]]
+    return integer
