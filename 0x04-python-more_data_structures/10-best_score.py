e31d105b055da53f0b6c0babc2c8ec15e722e595
Ousman-Busari <usman.traid@gmail.com>
2022-09-28 17:52:19 -0400

    minor fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict) and (a_dictionary is not None):
+    if is isinstance(a_dictionary, dict) and (a_dictionary is not None):
         max = 0
         for key in a_dictionary:
             if a_dictionary[key] > max:
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is not none and (is isintance(roman_string, str))
