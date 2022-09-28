de221107570076786145fe68daa2eb251faecec7
Ousman-Busari <usman.traid@gmail.com>
2022-09-28 18:27:34 -0400

    fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is not None and isinstance(a_dictionary, dict):
+    if a_dictionary is not None:
         return (sorted(a_dictionary.items(), key=lambda item: item[1]))[-1][0]
