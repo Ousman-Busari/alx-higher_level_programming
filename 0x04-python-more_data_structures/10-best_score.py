bfa57e0fa90a3172548502f9a84360d695dd3fdf
Ousman-Busari <usman.traid@gmail.com>
2022-09-28 17:15:57 -0400

    is not (!=) fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
-    if a_dictionary != None:
+    if a_dictionary is not None:
         max = 0
         for key in a_dictionary:
             if a_dictionary[key] > max:
