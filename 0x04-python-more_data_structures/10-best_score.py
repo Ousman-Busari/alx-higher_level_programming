e7cba2e193f7a412a2d4d2e41ce6220dfd7bcaa6
Ousman-Busari <usman.traid@gmail.com>
2022-09-28 19:28:18 -0400

    instance fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None or not isinstance(a_dictionary, dict):
+    if a_dictionary is None or not isinstance(a_dictionary, dict)\
+    or len(a_dictionary) == 0:
         return None
     return (sorted(a_dictionary.items(), key=lambda item: item[1]))[-1][0]
