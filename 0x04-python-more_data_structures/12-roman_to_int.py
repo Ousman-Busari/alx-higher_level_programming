d005346076f784b71e135ffca9df9b92e1203269
Ousman-Busari <usman.traid@gmail.com>
2022-09-28 19:08:57 -0400

    fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
+    roman_int = {'M': 100, 'D': 500, 'C': 100, 'L': 50,
+                     'X': 10, 'V': 5, 'I': 1}
+
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
+        if roman_int.get(roman_string[i], 0) == 0:
             return 0
 
     if roman_string is not None:
-        roman_int = {'M': 100, 'D': 500, 'C': 100, 'L': 50,
-                     'X': 10, 'V': 5, 'I': 1}
 
         roman_digits = list(roman_string)
         integer = 0
