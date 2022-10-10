cb458f553a121d81e604e23ce0a250602165bbec
Ousman-Busari <usman.traid@gmail.com>
2022-10-10 11:40:52 -0400

    style fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
         except IndexError:
-            break;
+            break
         else:
             print_count += 1
 
