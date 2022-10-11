b5a1a40fb67c828cf5edaecdd0dff10e3cdcf80b
Ousman-Busari <usman.traid@gmail.com>
2022-10-11 11:52:36 -0400

    private attribures

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """class Square with one attributes which id kept private"""
+    def __init__(self, size):
+        self.__size = size
