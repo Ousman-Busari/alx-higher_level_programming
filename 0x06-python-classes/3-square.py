5c7882bfd247e4f4b65d71b3f7df5806212d81d5
Ousman-Busari <usman.traid@gmail.com>
2022-10-11 13:29:55 -0400

    area method defined in class Square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""defines a class Square"""
+
+
+class Square:
+    """class representation of a Square
+
+    Attributes:
+               size(int): size of the side of the square
+    """
+    def __init__(self, size=0):
+        """Initiliazes an instance of the square
+
+        Args:
+             size(int): size of the side of the square
+
+        Returns:
+                None
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Calculates the area of the square
+
+        Return: area of the square
+        """
+        return (self.__size * self.__size)
