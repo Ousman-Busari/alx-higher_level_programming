dcb56f5ca24a34344cc51d0ea42534b460464e4f
Ousman-Busari <usman.traid@gmail.com>
2022-10-11 12:43:05 -0400

    pass arg to errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """class representation of a Square
+
+    Attributes:
+             __size(int): size of the side of the square
+    """
+    def __init__(self, size = 0):
+        """Initiliaze an instance of the square
+
+        Args:
+             size(int): size of the side of the square
+
+        Returns:
+                None
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
