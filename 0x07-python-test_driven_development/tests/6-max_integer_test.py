d63636c2a6ae81529794cdd6eea7ba203455c687
Ousman-Busari <usman.traid@gmail.com>
2022-10-20 10:10:05 -0400

    string sequence fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,7 +17,11 @@ class TestMaxInteger(unittest.TestCase):
         # Test the case where the length of the list is zero
         self.assertEqual(max_integer([]), None)
 
-    def test_list_of_integers(self):
+    def test_list_of_int_and_floats(self):
         # Test the case where all elements of a list are integers
         self.assertEqual(max_integer([4, 6, 3, 1, 7, 9, 8, 2]), 9)
-        self.assertEqual(max_integer([4, 6, 3, 1, 3, 4, 8, 2]), 8)
+        self.assertEqual(max_integer([4, 6, 3.8, 1.9, -5.6, 4.7, 8.5, 2]), 8.5)
+
+    def test_string_type(self):
+        # Test case for an argument of non-list type
+        self.assertEqual(max_integer("string"), 't')
