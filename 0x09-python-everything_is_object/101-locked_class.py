bf46d12aa007ff6d289758b39ce31695dc628bcf
Ousman-Busari <usman.traid@gmail.com>
2022-10-21 04:50:08 -0400

    Style fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module contaning a locked class """
 
+
 class LockedClass:
     """ A class with no dictionary, only a memory reservation
     for an attribte called first_name
