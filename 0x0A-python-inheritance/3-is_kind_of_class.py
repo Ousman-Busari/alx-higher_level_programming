699174f00947603918a68de31cc2ea6757296b13
Ousman-Busari <usman.traid@gmail.com>
2022-10-24 14:01:34 -0400

    more test cases added

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ contains the definition of the function is_kind-of_class """
 
+
 def is_kind_of_class(obj, a_class):
     """ Returns True if obj is a direct or indirect instance of a_class """
     if isinstance(obj, a_class):
