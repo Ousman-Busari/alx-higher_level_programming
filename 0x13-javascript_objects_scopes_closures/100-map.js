7a5ba66a423f07e3b63bc7db4be0245b56408854
Ousman <usman.traid@gmail.com>
2022-12-21 03:24:09 -0500

    code refactored

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const arr = require('./100-data.js').list;
-const newArray = arr.map(i => i * arr.indexOf(i));
+const newArray = arr.map((i, index) => i * index);
 console.log(arr);
 console.log(newArray);
