301f1556b95a964aca41006fab91d16c4df345a8
Ousman <usman.traid@gmail.com>
2023-01-17 06:37:45 -0500

    completed tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status"""
-
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    html = response.read()
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
 
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
