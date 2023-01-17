c1ea837f74f8604aa1b946201be4c7511456044c
Ousman <usman.traid@gmail.com>
2023-01-17 12:37:38 -0500

    handling json with requests package

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    arg = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": arg}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        if r.json() == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.json.get("id"), r.json.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
