8857fec8d50051421404ddfc3ed5c1ae7cde7527
Ousman <usman.traid@gmail.com>
2023-02-09 18:26:38 -0500

    code refactored to meet semistandard style

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,40 +2,24 @@
 // prints all characters of a Star Wars movie:
 
 const request = require('request');
-const film_url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+const filmURL = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+const pagePeople = 'https://swapi-api.alx-tools.com/api/people/';
 
-const pagePeople = "https://swapi-api.alx-tools.com/api/people/";
-// request(url, (error, response, body) => {
-//   if (!error) {
-//     const people = JSON.parse(body).results
-//     characters.forEach((character) => {
-//       request(url, (error, response, body) => {
-//         if (!error) {
-//           const characters = JSON.parse(body).characters;
-//           console.log(JSON.parse(body).name);
-//         }
-//       });
-//     });
-//   }
-// });
-
-
-function printCharacters(pagePeople) {
+function printCharacters (pagePeople) {
   request(pagePeople, (error, response, body) => {
     if (!error) {
-      const page = JSON.parse(body)
+      const page = JSON.parse(body);
       const people = JSON.parse(body).results;
       people.forEach(person => {
-        if (person.films.includes(film_url)) {
+        if (person.films.includes(filmURL)) {
           console.log(person.name);
         }
-      })
-      // console.log(page.next)
+      });
       if (page.next) {
         printCharacters(page.next);
       }
     }
-  })
+  });
 }
 
-printCharacters(pagePeople)
+printCharacters(pagePeople);
