cb76ae0267371e794196b10217cbcffbe6ecff77
Ousman <usman.traid@gmail.com>
2023-02-09 07:08:21 -0500

    code refactored

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,14 @@
 const request = require('request');
 
 const url = process.argv[2];
-// Wedge Antilles uri
-const uri = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (error, response, body) => {
-  console.log(error || JSON.parse(body)
-    .results
-    .map((episode) => episode.characters)
-    .filter((episodeCharacters) => episodeCharacters.includes(uri))
-    .length);
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, episode) => {
+      return episode.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
 });
